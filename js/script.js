2136d36d035a7b704bc2bb803f376dbbb69b173c
Mikal Dieatrick <Mdieatrick@gmail.com>
2022-10-15 23:56:49 -0700

    Completed main assignment functions
    added circleType.js
    Added Excon font (variable)
    made it my own

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 console.log("test");
+
+const circleType = new CircleType(document.getElementById("header"));
+
+var para1 = new CircleType(document.getElementById('para1'))
+  .radius(270);
+
+window.addEventListener('resize', function updateRadius() {
+    para1.radius(para1.element.offsetWidth / 2);
+});
+
+//despite existing in line 8, it freaks out about us calling it in line 13? i dont fully understand on this one
+//updateRadius();
